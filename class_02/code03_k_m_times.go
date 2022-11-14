72e4402a1c2fcf13b605daf12d5e66c16a285d4f
qianwj01 <qianwj01@mingyuanyun.com>
2022-11-14 18:39:16 +0800

    算法题

---
+++ class_02/code03_k_m_times.go
@@ -0,0 +1,12 @@
+package main
+
+// 在数组中，只有1种数出现了k次，其他数出现了m次，找到出现了k次的数，并返回
+// m > 1, 且 k < m
+// 要求：额外空间复杂度O(1)，时间复杂度O(N)
+//
+func onlyKTimes(arr []int, k int, m int) int {
+	return -1
+}
+
+func main() {
+}
