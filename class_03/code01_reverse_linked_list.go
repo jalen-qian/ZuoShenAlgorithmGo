5e28ffe4f9a8ed8e308a08bf0cac733bfcc574bb
qianwj01 <qianwj01@mingyuanyun.com>
2023-02-01 14:46:51 +0800

    算法题，反转单向链表

--- class_03/code01_reverse_linked_list.go
+++ class_03/code01_reverse_linked_list.go
@@ -9,30 +9,58 @@ import (
 func reverseLinkedList(head *utils.Node) *utils.Node {
 	var pre *utils.Node
 	var next *utils.Node
-	for head.Next != nil {
+	cur := head
+	for cur != nil {
 		// 先记住下一个节点的位置
-		next = head.Next
-		// 当前节点往前置
-		head.Next = pre
+		next = cur.Next
+		// 当前节点往前指
+		cur.Next = pre
 		// pre来到当前节点
-		pre = head
+		pre = cur
+		// 如果没有下一个节点了，head就定下来
+		if next == nil {
+			head = cur
+		}
 		// 当前节点往下一个节点跳
-		head = next
+		cur = next
+
 	}
 	return head
 }
 
+// test 构造一个新的单向链表，是原始链表反转的形式
+func test(head *utils.Node) *utils.Node {
+	if head == nil {
+		return nil
+	}
+	// 用一个数组来存
+	arr := make([]int, 0)
+	cur := head
+	for cur != nil {
+		arr = append(arr, cur.Value)
+		cur = cur.Next
+	}
+	// 反向构造
+	newHead := &utils.Node{Value: arr[len(arr)-1]}
+	cur = newHead
+	for i := len(arr) - 2; i >= 0; i-- {
+		cur.Next = &utils.Node{Value: arr[i]}
+		cur = cur.Next
+	}
+	return newHead
+}
+
 func copyLinkedList(node *utils.Node) *utils.Node {
 	if node == nil {
-		return node
+		return nil
 	}
 	newNode := &utils.Node{Value: node.Value}
-	old := node.Next
-	cur := newNode
-	for old != nil {
-		cur.Next = old
-		cur = cur.Next
-		old = old.Next
+	oldCur := node
+	newCur := newNode
+	for oldCur.Next != nil {
+		newCur.Next = &utils.Node{Value: oldCur.Next.Value}
+		oldCur = oldCur.Next
+		newCur = newCur.Next
 	}
 	return newNode
 }
@@ -50,8 +78,8 @@ func checkLinkedListEqual(head1 *utils.Node, head2 *utils.Node) bool {
 	if head1 == nil && head2 == nil {
 		return true
 	}
-	var cur1 = head1
-	var cur2 = head2
+	cur1 := head1
+	cur2 := head2
 	for cur1 != nil {
 		if cur2 == nil {
 			return false
@@ -66,15 +94,18 @@ func checkLinkedListEqual(head1 *utils.Node, head2 *utils.Node) bool {
 }
 
 func main() {
-	testTimes := 1
+	testTimes := 100000
 	for i := 0; i < testTimes; i++ {
 		// 测试方式：反转两次，测试反转后的结果是否与原始链表全等
 		linkedList := utils.GenerateRandomLinkedList(1000, -100, 100)
 		copyList := copyLinkedList(linkedList)
 		reversedList := reverseLinkedList(linkedList)
-		restore := reverseLinkedList(reversedList)
-		if !checkLinkedListEqual(copyList, restore) {
+		reversedList2 := test(copyList)
+		if !checkLinkedListEqual(reversedList, reversedList2) {
 			fmt.Println("Fucking fucked!!!")
+			fmt.Printf("原始链表：%s\n", utils.SPrintLinkedList(copyList))
+			fmt.Printf("反转链表：%s\n", utils.SPrintLinkedList(reversedList))
+			fmt.Printf("对数器链表：%s\n", utils.SPrintLinkedList(reversedList2))
 			return
 		}
 	}
--- utils/linked_list.go
+++ utils/linked_list.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -11,20 +12,42 @@ type Node struct {
 	Next  *Node
 }
 
+func genNodeValue(minValue int, maxValue int) int {
+	return rand.Intn(maxValue-minValue+1) + minValue
+}
+
 // GenerateRandomLinkedList 初始化一个随机的链表
 func GenerateRandomLinkedList(maxLength int, minValue int, maxValue int) *Node {
 	rand.Seed(time.Now().UnixNano())
 	length := rand.Intn(maxLength + 1)
-	var result *Node
-	var cur *Node
-	for i := 0; i < length; i++ {
-		value := rand.Intn(maxValue-minValue+1) + minValue
-		if result == nil {
-			result = &Node{Value: value}
-			cur = result
-		} else {
-			cur.Next = &Node{Value: value}
-		}
+	// 如果长度是0，则返回空链表
+	if length == 0 {
+		return nil
 	}
-	return result
+	// 先构造一个头节点
+	head := &Node{Value: genNodeValue(minValue, maxValue)}
+	cur := head
+	// 还剩length-1个节点
+	for i := 1; i < length; i++ {
+		value := genNodeValue(minValue, maxValue)
+		cur.Next = &Node{Value: value}
+		cur = cur.Next
+	}
+	return head
+}
+
+// SPrintLinkedList 打印链表
+func SPrintLinkedList(head *Node) string {
+	if head == nil {
+		return "null"
+	}
+	cur := head
+	// 遍历链表
+	ans := "{"
+	for cur != nil {
+		ans += fmt.Sprintf("%d->", cur.Value)
+		cur = cur.Next
+	}
+	ans += "null}"
+	return ans
 }
