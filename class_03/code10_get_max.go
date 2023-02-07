d7e4ca2b4fb745b8ce044bc1bb5b63603ac8faa4
jalen-qian <jalen_qian@163.com>
2023-02-07 23:09:55 +0800

    算法题:使用递归和二分思想获取数组中的最大值

---
+++ class_03/code10_get_max.go
@@ -0,0 +1,50 @@
+package main
+
+/*
+给定一个无序的数组，使用递归返回数组中的最大值，要求时间复杂度O(logN)
+
+思路：使用二分的思想递归，假设我设计了一个process函数，给定数组和一个区间，process会返回这个区间内的最大值
+
+我们假设给定的array的区间分别是 L 和 R (假设数组长度是n，则L=0，R=n-1)，我找到L和R的中点 mid
+则必然满足  maxValue = max(process(arr, L, mid), process(arr, mid+1, R))
+也就是将[L,mid]区间和[mid+1,R]区间分别用process求最大值，最终的最大值肯定是这两个值中较大的那个
+
+*/
+
+// GetMax 已知我们实现了process函数，则最终的结果是[0, n-1]上的最大值
+func GetMax(arr []int) int {
+	return process(arr, 0, len(arr)-1)
+}
+
+// process 就是要实现的函数，会返回arr在L~R上的最大值
+func process(arr []int, L, R int) int {
+	// 如果左右区间都相等了，只有一个数了，则返回这个数
+	if L == R {
+		return arr[L]
+	}
+	if L > R {
+		return 0
+	}
+	// 否则，找到中点
+	mid := L + (R-L)>>1
+	// 取这两个区间的最大值，返回
+	return max(process(arr, L, mid), process(arr, mid+1, R))
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+// GetMaxForTest 对数器，直接遍历找最大值
+func GetMaxForTest(arr []int) int {
+	var maxValue int
+	for _, v := range arr {
+		if v > maxValue {
+			maxValue = v
+		}
+	}
+	return maxValue
+}
---
+++ class_03/code10_get_max_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"ZuoShenAlgorithmGo/utils"
+	"testing"
+)
+
+func TestGetMax(t *testing.T) {
+	t.Log("测试开始")
+	testTimes := 200000
+	for i := 0; i < testTimes; i++ {
+		//t.Logf("测试第%d次", i+1)
+		// 生成一个随机数组
+		arr := utils.GenerateRandomSlice(1000, -1000, 1000)
+		max1 := GetMax(arr)
+		max2 := GetMaxForTest(arr)
+		if max1 != max2 {
+			t.Fatalf("获取到不同的最大值，max1:%d,max2:%d\n", max1, max2)
+		}
+	}
+	t.Log("测试结束")
+}
