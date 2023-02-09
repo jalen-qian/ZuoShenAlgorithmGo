71f5f781165901d4903c6b05a033ece3e7a141b6
qianwj01 <qianwj01@mingyuanyun.com>
2023-02-09 16:57:24 +0800

    算法题

--- class_03/code10_reverse_list_in_k_group.go
+++ class_03/code10_reverse_list_in_k_group.go
@@ -7,7 +7,7 @@ k 是一个正整数，它的值小于或等于链表的长度。如果节点总
 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
 实例1：k = 2
 1->2->3->4->5->null
-2->1->3->4->5->null
+2->1->4->3->5->null
 实例2：k = 3
 1->2->3->4->5->null
 3->2->1->4->5->null
@@ -42,22 +42,29 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 	if head == nil {
 		return nil
 	}
-	// 1. 先找到第一个分组的end节点，作为整个新链表的头结点
+	// 先找到第一个分组的end节点，作为整个新链表的头结点
 	end := findGroupEnd(head, k)
 	// 如果返回nil，说明整个链表都不够一组，直接返回当前头结点
 	if end == nil {
 		return head
 	}
+	// 分组开始的节点，最开始是头结点
 	start := head
+	// 找到第一个分组的结束节点后，由于会反转，这个结束节点就会是最终的头部
 	head = end
-
+	// 用户反转链表的pre和next以及cur
 	var pre *ListNode
 	var next *ListNode
+	// cur先来到第一个分组的头结点
 	cur := start
+	// preEnd用来记录上一个分组的尾部，对于第一个分组来说，尾部就是头结点（会反转）
+	preEnd := start
 	// 2. 不断找分组的end节点，如果不是空的，说明找到一个分组
 	for end != nil {
+		// endNext用来先记住下一个分组的头结点，反转后好连过去
 		endNext := end.Next
-		// 反转组内链表，一共进行次数是每组节点个数
+		// 反转组内链表，一共进行次数是k次
+		pre = nil
 		for i := 0; i < k; i++ {
 			// 先记住下一个节点
 			next = cur.Next
@@ -68,10 +75,17 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 			// 当前来到下一个位置
 			cur = next
 		}
-		// 当前分组的最后一个节点指向下一个分组的头节点
+		// 当前分组的最后一个节点（反转后start来到尾部）指向下一个分组的头节点
 		start.Next = endNext
-		// start和end来到下一个分组
+		// 上一个分组的尾结点指向当前分组的头结点（反转后end变成头结点）
+		if preEnd != nil {
+			preEnd.Next = end
+		}
+		// preEnd跳到当前分组的尾部
+		preEnd = start
+		// start来到下一个分组的头结点
 		start = endNext
+		// end 继续获取下一个分组的尾结点
 		end = findGroupEnd(start, k)
 	}
 	return head
--- class_03/code10_reverse_list_in_k_group_test.go
+++ class_03/code10_reverse_list_in_k_group_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestReverseListInKGroup(t *testing.T) {
 	head := GenerateLinkedListBySlice([]int{1, 2, 3, 4, 5})
-	ans := reverseKGroup(head, 2)
+	ans := reverseKGroup(head, 3)
 	fmt.Printf("结果:%s\n", SPrintLinkedList(ans))
 	//head = GenerateLinkedListBySlice([]int{1, 2, 3, 4, 5})
 	//ans = reverseKGroup(head, 2)
