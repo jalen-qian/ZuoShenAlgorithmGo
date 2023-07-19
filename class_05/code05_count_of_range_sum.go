487b037d39dfb6e9d8b91f2ba5e35437153d228e
qianwj01 <qianwj01@mingyuanyun.com>
2023-07-19 20:16:17 +0800

    区间和个数问题

---
+++ class_05/code05_count_of_range_sum.go
@@ -0,0 +1,15 @@
+package class_05
+
+/**
+区间和的个数问题：
+给定一个整数数组 nums 和两个整数 lower 和 upper。求数组中，值位于范围[lower,upper]（包含lower和upper）之内的区间和的个数。
+
+区间和的概念：s(i,j) 表示在 nums 中，位置从i到j的元素之和，包含 i和j,(i <= j)
+
+leetcode 原题：https://leetcode.cn/problems/count-of-range-sum/
+*/
+
+// countRangeSum 区间和个数问题
+func countRangeSum(nums []int, lower int, upper int) int {
+	return 0
+}
