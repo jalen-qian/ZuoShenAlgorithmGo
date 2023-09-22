5aeb6f0e2f112f79d128a7b684157b0380fc187d
jalen-qian <jalen_qian@163.com>
2023-09-22 11:36:47 +0800

    二叉树

--- class_13/code06_max_distance.go
+++ class_13/code06_max_distance.go
@@ -15,15 +15,17 @@ func distanceProcess(x *TreeNode) (maxDistance int, height int) {
 	if x == nil {
 		return
 	}
-	// 左子树最大距离与高度
+	// 递归获取左子树最大距离与高度
 	leftMaxDistance, leftHeight := distanceProcess(x.Left)
+	// 递归获取右子树最大距离与高度
 	rightMaxDistance, rightHeight := distanceProcess(x.Right)
-	// 如果经过x,最大距离
+	// 如果经过x,最大距离是左右高度相加再加1
 	maxDistance = leftHeight + rightHeight + 1
+	// 如果不经过x,最大距离是左右子树最大距离的较大者
 	maxDistance = utils.Max(leftMaxDistance, maxDistance)
 	maxDistance = utils.Max(rightMaxDistance, maxDistance)
-
-	// 当前树最大高度 = 左右子树最大高度 + 1
+	// 当前树最大高度 = 左右子树高度最大值 + 1
 	height = utils.Max(leftHeight, rightHeight) + 1
+	// 返回当前x的最大距离和高度
 	return
 }
